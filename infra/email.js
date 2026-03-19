3b9e6db74d8797971783577b9bec00f11bd01ba9
MGLMendes <miguelmendes203@gmail.com>
2026-03-19 12:10:00 -0300

    fix: adjust password email variable

--- infra/email.js
+++ infra/email.js
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
   port: process.env.EMAIL_SMTP_PORT,
   auth: {
     user: process.env.EMAIL_SMTP_USER,
-    pass: process.env.EMAIL_SMTP_PASSWOR,
+    pass: process.env.EMAIL_SMTP_PASSWORD,
   },
   secure: process.env.NODE_ENV === "production" ? true : false,
 });
